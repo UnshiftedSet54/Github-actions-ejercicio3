4319093eda26bb03d94c4ffe2d39773fede08506
UnshiftedSet54 <smlrossales@gmail.com>
2024-04-02 19:27:08 +0100

    updated tests

--- app.test.js
+++ app.test.js
@@ -4,19 +4,19 @@ const request = supertest(app)
 
 describe("Test endpoints", () => {
   test('Debe devolver "Books"', async () => {
-    const response = request.get('/books')
+    const response = await request.get('/books')
     const body = JSON.parse(response.text)
     expect(response.status).toBe(200)
     expect(body.msg).toBe('Books')
   })
   test('Debe devolver "Authors"', async () => {
-    const response = request.get('/authors')
+    const response = await request.get('/authors')
     const body = JSON.parse(response.text)
     expect(response.status).toBe(200)
     expect(body.msg).toBe('Authors')
   })
   test('Debe devolver error', async () => {
-    const response = request.get('/any-route')
+    const response = await request.get('/any-route')
     const body = JSON.parse(response.text)
     expect(response.status).toBe(404)
     expect(body.msg).toBe('Not Found')
